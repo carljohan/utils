3c9ccc171f4f1372b4e36cfb789acef6489e5c40
carljohan <carljohan@hey.com>
2024-03-15 21:34:38 -0400

    fix: explicit return types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export async function wait(milliseconds: number) {
+export async function wait(milliseconds: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs))
 }
